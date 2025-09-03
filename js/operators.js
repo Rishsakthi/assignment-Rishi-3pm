2c75a4879741d3cb1a77e90fb1aa9ade97e9c30b
Rishi <rishisr0126g@gmail.com>
2025-09-03 20:54:43 +0530

    new assignment

---
+++ js/operators.js
@@ -0,0 +1,104 @@
+// arithmetic operators
+let a ,b;
+a=3;
+b=4;
+console.log(a+b);
+a=10;
+b=3;
+console.log(a-b);
+a=7;
+b=1;
+console.log(a*b);
+a=56;
+b=8;
+console.log(a/b);
+a=55;
+b=8;
+console.log(a%b);
+
+
+let c,d;
+c=2;
+d=5;
+console.log(c+d);
+c=9;
+d=2;
+console.log(c-d);
+c=1;
+d=7;
+console.log(c*d);
+c=63;
+d=9;
+console.log(c/d);
+c=61;
+d=9;
+console.log(c%d);
+
+// assignment operators
+let ab ,bc;
+ab=3;
+ab +=4;
+console.log(ab);
+bc=4;
+bc+=3;
+console.log(bc);
+
+ab=10;
+ab -=3;
+console.log(ab);
+bc=9;
+bc-=2;
+console.log(bc);
+
+ab=7;
+ab*=1;
+console.log(ab);
+bc=1;
+bc*=7;
+console.log(bc);
+
+ab=14;
+ab/=2;
+console.log(ab);
+bc=21;
+bc/=3;
+console.log(bc);
+
+// comparison
+a=4;
+b=7;
+console.log(a == b);
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+console.log(a !=b);
+
+// logical operators
+a=5;
+b=3;
+console.log(a>6 && b>5);
+console.log(a>=5 || b>3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
